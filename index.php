ea3810a5a58064ec2a3dcc22f596b9c10b3d0aa0
Emmanuek5 <83721168+Emmanuek5@users.noreply.github.com>
2021-12-13 18:56:19 +0100

    delete

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+include('./connections/config.php');
 
 
 
@@ -9,9 +11,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Bulk Mailer</title>
 </head>
 <body>
-    
+    <form action="">
+        
+    </form>
 </body>
 </html>
