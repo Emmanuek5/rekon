832bf6064b727e32cd156251159c74827f8048bd
Emmanuek5 <83721168+Emmanuek5@users.noreply.github.com>
2021-12-13 18:03:23 +0100

    1st Commit

--- connections/config.php
+++ connections/config.php
@@ -1,6 +1,15 @@
 <?php
-$host 
+$host =   "localhost";
+$user = "root";
+$pass = "";
+$database = "rekon";
 
+$con = mysqli_connect($host, $user, $pass, $database);
+
+if (!$con) {
+ echo "Error";
+
+}
 
 
 
